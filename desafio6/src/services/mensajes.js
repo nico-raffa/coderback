1af0d402cbf5c425e0fe9507074ab4b4997a73c7
Nicolas <djnikorc@gmail.com>
2022-03-25 14:45:34 -0300

    Desafio 6 - websocket

--- desafio6/src/services/mensajes.js
+++ desafio6/src/services/mensajes.js
@@ -6,7 +6,8 @@ class ContMensajes {
     }
     async save (mensaje){
         try{
-            if (this.data.length >= 0){  
+            this.data = await this.getAll()
+            if (this.data.length !== 0){  
                 mensaje.timestamp = new Date().toLocaleString()
                 this.data.push(mensaje)
                 fs.promises.writeFile(this.archiveName,JSON.stringify(this.data,null,2))}
@@ -17,39 +18,6 @@ class ContMensajes {
             console.log(`Error en escritura: ${error}`)
         }
     }
-    async getById(id){
-        try{
-            const objs = await this.getAll()
-            const findObj = objs.find(obj => obj.id === Number(id))
-            return findObj
-        }catch(error){
-            console.log(`Error al leer ID ${error}`)
-        }
-    }
-    async updateById(id, newProduct){
-        try {
-            const list = await this.getAll()
-            const old = list.findIndex(product=> product.id == id)
-            let product = list[old]
-            if (product){
-                const {name, thumbnail, price, description, code, stock} = newProduct
-                product.name = name;
-                product.description = description;
-                product.code = code;
-                product.stock = stock;
-                product.thumbnail = thumbnail;
-                product.price = price;
-                list[index] = product;
-                await this.save(list)
-            }else{
-                return null
-            }
-
-            
-        } catch (error) {
-            return (error)            
-        }
-    }
     async getAll(){
         try{
             const data = await fs.promises.readFile(this.archiveName,"utf-8")
@@ -57,19 +25,6 @@ class ContMensajes {
         }catch(error){
             return (`Error en lectura: ${error}`)
         }
-    }
-    async deleteById(id){
-        const objs = await this.getAll()
-        const findObj = objs.findIndex(obj => obj.id === id)
-        if (findObj === -1){
-            console.log(`Error al borrar, no se encontro el ID ${id}`)
-        }
-        objs.splice(findObj,1)
-        try{
-            await fs.promises.writeFile(this.archiveName,JSON.stringify(objs,null,2))
-        }catch(error){
-            console.log(`Error al intentar escribir el archivo ${error}`)
-        }          
     }
     async deleteAll(){
         await fs.promises.unlink(this.archiveName)
